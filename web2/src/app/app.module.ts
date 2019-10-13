3fe1ccd59a1084a0b5b847be8a4ce5306f2a6cfc
Javier López-Contreras <38789151+javierlc2000@users.noreply.github.com>
2019-10-13 03:31:48 +0200

    voting

--- web2/src/app/app.module.ts
+++ web2/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { InitScreenComponent } from './init-screen/init-screen.component';
 import { UserScreenComponent } from './user-screen/user-screen.component';
 import { CentralContainerComponent } from './init-screen/central-container/central-container.component';
 import { HeaderComponent } from './user-screen/header/header.component';
-import { ScrollEmbedderComponent } from './user-screen/scroll-embedder/scroll-embedder.component';
 import { ScrollComponent } from './user-screen/scroll-embedder/scroll/scroll.component';
 import { LoginBoxComponent } from './init-screen/central-container/login-box/login-box.component';
 import { RegisterBoxComponent } from './init-screen/central-container/register-box/register-box.component';
@@ -21,7 +20,6 @@ import {
 
 import { HttpClientModule } from '@angular/common/http';
 import { DialogDataExampleComponent, DialogDataExampleComponentDialog } from './user-screen/header/dialog-data-example/dialog-data-example.component';
-import { ScrollCardComponent } from './user-screen/scroll-embedder/scroll/scroll-card/scroll-card.component';
 import { DialogOutComponent, DialogOutComponentDialog } from './user-screen/header/dialog-out/dialog-out.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { CommunityComponent } from './community/community.component';
@@ -33,11 +31,10 @@ import { CommunityComponent } from './community/community.component';
     UserScreenComponent,
     CentralContainerComponent,
     HeaderComponent,
-    ScrollEmbedderComponent,
     ScrollComponent,
     LoginBoxComponent,
     RegisterBoxComponent,
-    DialogDataExampleComponent, DialogDataExampleComponentDialog, ScrollCardComponent,
+    DialogDataExampleComponent, DialogDataExampleComponentDialog,
     DialogOutComponent, DialogOutComponentDialog, FeedbackComponent, CommunityComponent
   ],
   imports: [
--- web2/src/app/user-screen/scroll-embedder/scroll/scroll.component.ts
+++ web2/src/app/user-screen/scroll-embedder/scroll/scroll.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HTTPLessonService, FeedbackResponse, InfoUserResponse, ScheduleResponse } from '../../httplesson.service'
 
 @Component({
   selector: 'app-scroll',
@@ -6,15 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scroll.component.scss']
 })
 export class ScrollComponent implements OnInit {
+  @Input() username: string;
+
+  prod = [0, 0, 0, 0, 0];
   ITER = [0, 1, 2, 3, 4];
   cards = ["Algorithms", "Complex analysis", "Linear Optimization", "Parametrized Complexity", "Theory"];
   votes = [0,             5,                    9,                    3,                        5];
   it = 0;
   n = 5;
 
-  constructor() { }
+  constructor(private less: HTTPLessonService) {}
 
   ngOnInit() {
+    var _this = this;
+    this.less.infoUser(this.username).subscribe(data => {
+      _this.n = data.subject.length;
+      _this.cards = data.subject;
+      _this.votes = data.feedback;
+      _this.prod = Array(_this.n).fill(0);
+      _this.ITER = Array.from(Array(_this.n).keys());
+    });
   }
 
   expandLess() {
@@ -23,5 +35,16 @@ export class ScrollComponent implements OnInit {
   expandMore() {
     this.it++;
   }
-
+  vote(i: number) {
+    this.less.sendFeedback(this.username, this.prod[i], this.cards[i]).subscribe();
+    this.votes[i] = this.prod[i];
+    this.prod[i] = 0;
+  }
+  unvote(i: number) {
+    this.less.sendFeedback(this.username, 0, this.cards[i]).subscribe();
+    this.votes[i] = 0;
+  }
+  prop(i: number, e: any) {
+    this.prod[i] = e.value;
+  }
 }
